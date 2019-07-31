e1b56823abcb838653dc003ce0378e27015d3433
Xarv <strzelecki20@gmail.com>
2019-08-01 00:31:30 +0200

    css + delete start

---
+++ postFilter.js
@@ -0,0 +1,9 @@
+// let deletePost = document.getElementById("deletePost");
+
+// chrome.storage.sync.get("display", function(data) {
+//   deletePost.style.display = data.display;
+// });
+
+// deletePost.onclick = function(element){
+//     let delete =
+// }
