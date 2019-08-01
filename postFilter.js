c48c98b2e96e7696b9c64a85e349f105b7f263bd
Xarv <strzelecki20@gmail.com>
2019-08-01 12:41:34 +0200

    so close (CORB)

--- postFilter.js
+++ postFilter.js
@@ -2,9 +2,33 @@ chrome.runtime.onMessage.addListener(gotMessage);
 function gotMessage(message, sender, sendresponse) {
   console.log(message.txt);
   let posts = document.querySelectorAll("div.articles__summary");
-  let i = 1;
-  for (post of posts) {
-    if (i % 2 == 0) post.style["display"] = "none";
-    i++;
-  }
+  let post = document.querySelectorAll("h2.articles__h2 a");
+  test = link => {
+    let block;
+    const API = `https://g40zr.mocklab.io/blockFilter/${link}`;
+    console.log("link " + API);
+    fetch(API)
+      .then(response => {
+        if (response.status === 200) return response;
+        throw Error(response.status);
+      })
+      .then(response => response.text())
+      .then(data => {
+        block = JSON.parse(data.block);
+        console.log(block.block);
+      })
+      .catch(error =>
+        error == "Error: 404"
+          ? alert("Not found")
+          : alert("Please try later " + error)
+      );
+  };
+  console.log(post[1].href);
+  console.log(test(post[1].href));
+  let i = 0;
+  // for (post of posts) {
+  //   if (i % 2 == 0) post.style["display"] = "none";
+  //   console.log(post);
+  //   i++;
+  // }
 }
