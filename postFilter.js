6dd6976465f1dc6b4e1ab839b219bfe78adcb900
Xarv <strzelecki20@gmail.com>
2019-08-01 10:32:24 +0200

    delete even post (correct)

--- postFilter.js
+++ postFilter.js
@@ -2,7 +2,9 @@ chrome.runtime.onMessage.addListener(gotMessage);
 function gotMessage(message, sender, sendresponse) {
   console.log(message.txt);
   let posts = document.querySelectorAll("div.articles__summary");
+  let i = 1;
   for (post of posts) {
-    post.style["display"] = "none";
+    if (i % 2 == 0) post.style["display"] = "none";
+    i++;
   }
 }
