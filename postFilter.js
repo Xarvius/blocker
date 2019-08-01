bf192841ed1b4f0f4aa3c178bb479e0cf374670d
Xarv <strzelecki20@gmail.com>
2019-08-01 15:15:15 +0200

    repair CORB problem

--- postFilter.js
+++ postFilter.js
@@ -4,9 +4,10 @@ function gotMessage(message, sender, sendresponse) {
   let posts = document.querySelectorAll("div.articles__summary");
   let postLink = document.querySelectorAll("h2.articles__h2 a");
   let filterRespond;
-  filterAsk = link => {
-    const API = `https://g40zr.mocklab.io/blockFilter/${link}`;
-    fetch(API)
+
+  filterAsk = (link, post) => {
+    const API = `https://cors-anywhere.herokuapp.com/https://g40zr.mocklab.io/blockFilter/${link}`;
+    fetch(API, { mode: "cors" })
       .then(response => {
         if (response.status === 200) return response;
         throw Error(response.status);
@@ -14,19 +15,15 @@ function gotMessage(message, sender, sendresponse) {
       .then(response => response.text())
       .then(data => {
         filterRespond = JSON.parse(data);
-        return filterRespond.block;
+        if (filterRespond.block) post.style["display"] = "none";
       })
       .catch(error =>
-        error == "Error: 404"
-          ? console.log("Not found")
-          : console.log("Please try later " + error)
+        error == "Error: 404" ? console.log("Not found") : console.log(error)
       );
   };
   let i = 0;
   for (post of posts) {
-    // if (i % 2 == 0) pos.style["display"] = "none";
-    filterAsk(postLink[i].href);
-    if (filterRespond.block) post.style["display"] = "none";
+    filterAsk(postLink[i].href, post);
     i++;
   }
 }
