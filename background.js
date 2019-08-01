8991336c6748d76cb29736f6660a48d4ae1664d4
Xarv <strzelecki20@gmail.com>
2019-08-01 10:26:44 +0200

    simpler background

--- background.js
+++ background.js
@@ -1,17 +1,7 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({ display: none }, function() {
-    console.log("Rdy to filter");
-  });
-});
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-  chrome.declarativeContent.onPageChanged.addRules([
-    {
-      conditions: [
-        new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: { hostEquals: "steemit.com" }
-        })
-      ],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }
-  ]);
-});
+chrome.browserAction.onClicked.addListener(IconClicked);
+function IconClicked(tab) {
+  const msg = {
+    txt: "Deleted"
+  };
+  chrome.tabs.sendMessage(tab.id, msg);
+}
