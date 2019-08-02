ee02e573a9fcc9bd197a1f26bc3ce2f780bd5578
Xarvius <strzelecki20@gmail.com>
2019-08-02 14:40:25 +0200

    create dataCheck and urlSplit function

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const port = process.env.PORT || 3000;
 const dsteem = require("dsteem");
 let opts = {};
-
+const users = ["bue", "nate-atkins"];
 //connect to production server
 opts.addressPrefix = "STM";
 opts.chainId =
@@ -21,39 +21,43 @@ async function queryVotes(permlink, author) {
     console.log(err);
   });
 }
-const user = ["bue", "nate-atkins"];
+function urlSplit(url) {
+  let reqUrl = url.split("/").reverse();
+  reqUrl[1] = reqUrl[1].substr(1);
+  return reqUrl;
+}
+async function dataCheck(reqUrl) {
+  let data = await queryVotes(reqUrl[0], reqUrl[1]);
+  let test = -1;
+  let temp = -1;
+  let end = JSON.stringify({ block: false });
+  for (let j = 0; j < data[0].active_votes.length; j++) {
+    for (let i = 0; i < users.length; i++) {
+      test = data[0].active_votes[j].voter.indexOf(users[i]);
+      if (test > -1) {
+        temp = test;
+        break;
+      }
+    }
+    if (temp !== -1) {
+      end = JSON.stringify({ block: true });
+      break;
+    }
+  }
+  return end;
+}
+
 http
   .createServer(async (req, res) => {
-    let reqUrl;
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Request-Method", "*");
     res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
     res.setHeader("Access-Control-Allow-Headers", "*");
     res.writeHead(200, { "Content-Type": "application/json" });
-    if (req.url != "/favicon.ico") {
-      reqUrl = req.url.split("/").reverse();
-      reqUrl[1] = reqUrl[1].substr(1);
-    }
-    let data;
-    let end = JSON.stringify({ block: false });
-    if (reqUrl) {
-      data = await queryVotes(reqUrl[0], reqUrl[1]);
-      let test = -1;
-      let temp = -1;
-      for (let j = 0; j < data[0].active_votes.length; j++) {
-        for (let i = 0; i < user.length; i++) {
-          test = data[0].active_votes[j].voter.indexOf(user[i]);
-          if (test > -1) {
-            temp = test;
-            break;
-          }
-        }
-        if (temp !== -1) {
-          end = JSON.stringify({ block: true });
-          break;
-        }
-      }
-    }
+
+    const reqUrl = urlSplit(req.url);
+    const end = await dataCheck(reqUrl);
+
     res.end(end);
   })
   .listen(port, "127.0.0.1");
