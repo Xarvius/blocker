2d7bec0d2c28bd6f167aebef3da3271b9c40c2e9
Xarvius <strzelecki20@gmail.com>
2019-08-04 10:52:31 +0200

    rewrite postFilter

--- postFilter.js
+++ postFilter.js
@@ -1,29 +1,19 @@
 chrome.runtime.onMessage.addListener(gotMessage);
 function gotMessage(message) {
   console.log(message.txt);
-
-  filterAsk = (link, post) => {
-    const API = `http://localhost:3000/${link}`;
-    fetch(API, { mode: "cors" })
-      .then(response => {
-        if (response.status === 200) return response;
-        throw Error(response.status);
-      })
-      .then(response => response.text())
-      .then(data => {
-        let filterRespond;
-        filterRespond = JSON.parse(data);
-        if (filterRespond.block) post.style["display"] = "none";
-      })
-      .catch(error =>
-        error == "Error: 404" ? console.log("Not found") : console.log(error)
-      );
-  };
   let posts = document.querySelectorAll("div.articles__summary");
-  let postLink = document.querySelectorAll("h2.articles__h2 a");
-  let i = 0;
-  for (post of posts) {
-    filterAsk(postLink[i].href, post);
-    i++;
+
+  async function blockPosts(post) {
+    let link = post.querySelector("h2.articles__h2 a").href;
+    const API = `http://localhost:3000/${link}`;
+    let response = await fetch(API, { mode: "cors" });
+    let json = await response.json();
+
+    if (json.block) {
+      post.style["display"] = "none";
+      console.log(`Post ${link} - blocked`);
+    }
   }
+
+  Array.prototype.slice.call(posts).map(blockPosts);
 }
