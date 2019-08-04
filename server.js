a594f54fdb81a10e397665069d2f2493b1d5d0dc
Xarvius <strzelecki20@gmail.com>
2019-08-04 10:32:24 +0200

    rewrite dataCheck(), modify getDiscussions

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const port = process.env.PORT || 3000;
 const dsteem = require("dsteem");
+const blacklist = ["bue", "nate-atkins", "therising"];
 const client = new dsteem.Client("https://api.steemit.com");
 async function queryVotes(permlink, author) {
   const query = {
@@ -10,33 +11,22 @@ async function queryVotes(permlink, author) {
     start_permlink: permlink,
     truncate_body: 1
   };
-  return await client.database.getDiscussions("trending", query).catch(err => {
-    console.log(err);
-  });
+  return await client.database
+    .getDiscussions("trending", query)
+    .then(data => data[0].active_votes.map(list => list.voter))
+    .catch(err => err);
 }
 function urlSplit(url) {
   let reqUrl = url.split("/").reverse();
-  reqUrl[1] = reqUrl[1].substr(1);
+  reqUrl[1] = reqUrl[1].substr(1); //substr @
   return reqUrl;
 }
 async function dataCheck(reqUrl) {
   let data = await queryVotes(reqUrl[0], reqUrl[1]);
-  let test = -1;
-  let temp = -1;
-  let end = JSON.stringify({ block: false });
-  for (let j = 0; j < data[0].active_votes.length; j++) {
-    for (let i = 0; i < users.length; i++) {
-      test = data[0].active_votes[j].voter.indexOf(users[i]);
-      if (test > -1) {
-        temp = test;
-        break;
-      }
-    }
-    if (temp !== -1) {
-      end = JSON.stringify({ block: true });
-      break;
-    }
-  }
+  let block = data.some(voter => blacklist.includes(voter));
+  let end = block
+    ? JSON.stringify({ block: false })
+    : JSON.stringify({ block: true });
   return end;
 }
 
